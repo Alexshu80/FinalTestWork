5fedd6fe94a1653f5697c18c2df34e5a4f859c23
Alexander Shushlin <alexshushlin@gmail.com>
2022-10-10 13:32:43 +0300

    Finished the program

--- Program.cs
+++ Program.cs
@@ -18,3 +18,39 @@ void ShowArray(string[] array)
     }
     Console.WriteLine("]");
 }
+
+// Задание размера массива
+Console.Write("Сколько элементов будете вводить?: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+// Создание массива
+string[] array = new string[size];
+
+// цикл на заполнение массива и поиска количества элементов с длиной меньше или равной 3 символов
+int newsize = 0;
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"Введите {i + 1} элемент: ");
+    array[i] = Console.ReadLine() ?? "";
+    if (array[i].Length <= 3) newsize++;
+}
+
+// Создание нового массива размером равному количеству элементов с длиной меньше или равной 3 элементов
+string[] newArray = new string[newsize];
+
+// Цикл на заполнение нового массива элементами удовлетворяющими условию задачи
+int j = 0;
+for (int i = 0; i < size; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[j] = array[i];
+        j++;
+    }
+}
+
+// Вывод результатов на экран
+Console.WriteLine("Вы ввели массив:");
+ShowArray(array);
+Console.WriteLine("Новый массив:");
+ShowArray(newArray);
